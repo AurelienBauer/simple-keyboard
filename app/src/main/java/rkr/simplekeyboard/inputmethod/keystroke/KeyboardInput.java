9bfdbe74ad7839c4f93ff1a45a890517e00e77c9
aurelien.bauer <aurelien.aurelien.bauer@epitech.eu>
2019-05-27 18:19:32 +0100

    register these data on the json file: "randnbr#" : [   {     "NoKeyPressDelay": 2.9877, // Before pressing this key     "primaryCode": 116,     "x": 3,     "y": 5,     "KeyPressDelay": 0.9877 //While a key is press   } ]

--- app/src/main/java/rkr/simplekeyboard/inputmethod/keystroke/KeyboardInput.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/keystroke/KeyboardInput.java
@@ -1,5 +1,7 @@
 package rkr.simplekeyboard.inputmethod.keystroke;
 
+import org.json.JSONObject;
+
 public class KeyboardInput implements KeystrokeActionListener {
 
     private final KeystrokeManager ksManger;
@@ -9,12 +11,14 @@ public class KeyboardInput implements KeystrokeActionListener {
     }
     private long press_key_time = 0;
     private long release_key_time = 0;
+    private JSONObject jo;
 
     @Override
     public void onPressKey(int primaryCode, int repeatCount, boolean isSinglePointer) {
+        jo = new JSONObject();
         press_key_time = System.nanoTime();
         try {
-            ksManger.jo.put("NoKeyPressDelay", (press_key_time - release_key_time) / 1e6);
+            jo.put("NoKeyPressDelay", (press_key_time - release_key_time) / 1e6);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -24,7 +28,8 @@ public class KeyboardInput implements KeystrokeActionListener {
     public void onReleaseKey(int primaryCode, boolean withSliding) {
         release_key_time = System.nanoTime();
         try {
-            ksManger.jo.put("KeyPressDelay", (release_key_time - press_key_time) / 1e6);
+            jo.put("KeyPressDelay", (release_key_time - press_key_time) / 1e6);
+            ksManger.addJsonElem(jo);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -33,7 +38,7 @@ public class KeyboardInput implements KeystrokeActionListener {
     @Override
     public void onCodeInput(int primaryCode, int x, int y, boolean isKeyRepeat) {
         try {
-            ksManger.jo.put("primaryCode", primaryCode)
+            jo.put("primaryCode", primaryCode)
                     .put("x", x)
                     .put("y", y);
         } catch (Exception e) {
--- app/src/main/java/rkr/simplekeyboard/inputmethod/keystroke/KeystrokeManager.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/keystroke/KeystrokeManager.java
@@ -2,8 +2,11 @@ package rkr.simplekeyboard.inputmethod.keystroke;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Random;
+
 import android.content.Context;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
@@ -12,44 +15,62 @@ public class KeystrokeManager {
     final private Context context;
     private FileOutputStream outputStream;
     private static final String FILENAME = "keystrokePatter.json";
-    public JSONObject jo;
+    private JSONArray ja;
+    private File file;
+    private int newInput;
+    private final Random rand;
 
     public KeystrokeManager(Context context) {
         this.context = context;
+        rand = new Random();
+        rand.setSeed(System.nanoTime());
     }
 
     public void onCreate() {
-        System.out.println("Keystroke Mamager : onCreate.");
-        File file = new File(context.getFilesDir(), FILENAME);
+        try {
+            //             System.out.println("Keystroke Mamager : onCreate.");
+            file = new File(context.getFilesDir(), FILENAME); //create the file if it doesn't exist
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    public void addJsonElem(JSONObject new_jo) {
+        ja.put(new_jo);
+    }
     //Called to inform the input method that text input has started in an editor.
     public void onStartInput() {
-        System.out.println("Keystroke Mamager : onStartInput.");
+        //              System.out.println("Keystroke Mamager : onStartInput.");
         try {
-            outputStream = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
-            jo = new JSONObject();
+            newInput = rand.nextInt();
+            outputStream = new FileOutputStream(file, true);
+            ja = new JSONArray();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    /*
-    public void newEntry(final String pattern) {
-        try {
-            outputStream.write(pattern.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
 
     public void onDestroy() {
-        System.out.println("Keystroke Mamager : onDestroy.");
+        //          System.out.println("Keystroke Mamager : onDestroy.");
     }
 
     public void onFinishInput() {
-        System.out.println("Keystroke Mamager : onFinishInput.");
+        //          System.out.println("Keystroke Mamager : onFinishInput.");
         try {
-            outputStream.write(jo.toString().getBytes());
+            if (ja.length() > 0) {
+                JSONObject jo = new JSONObject();
+                jo.put(newInput+"#", ja);
+
+
+                String str = jo.toString();
+                str = str.substring(0, str.length() - 1);
+                // Concat two json object
+                if (file.length() > 0) {
+                    str = str.replaceFirst("\\{", ",");
+                }
+
+                outputStream.write(str.getBytes());
+            }
             outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
