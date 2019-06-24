42fe2ab0c4c27ebaeea181f41f5cd33547073943
aurelien.bauer <aurelien.aurelien.bauer@epitech.eu>
2019-06-24 14:22:34 +0100

    remove unused libraries

--- app/src/main/java/rkr/simplekeyboard/inputmethod/touchservice/TouchManager.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/touchservice/TouchManager.java
@@ -2,7 +2,6 @@ package rkr.simplekeyboard.inputmethod.touchservice;
 
 import android.annotation.SuppressLint;
 import android.app.IntentService;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Build;
@@ -16,13 +15,9 @@ import android.widget.LinearLayout;
 public class TouchManager extends IntentService implements View.OnTouchListener {
 
     private String TAG = this.getClass().getSimpleName();
-    // window manager
     private WindowManager mWindowManager;
-    // linear layout will use to detect touch event
     private LinearLayout touchLayout;
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     */
+
     public TouchManager() {
         super("SwipeScrollCatcherService");
     }
@@ -38,17 +33,13 @@ public class TouchManager extends IntentService implements View.OnTouchListener 
         super.onCreate();
         // create linear layout
         touchLayout = new LinearLayout(this);
-        // set layout width 30 px and height is equal to full screen
+
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
         touchLayout.setLayoutParams(lp);
-        // set color if you want layout visible on screen
-        //touchLayout.setBackgroundColor(Color.CYAN);
-        // set on touch listener
+
         touchLayout.setOnTouchListener(this);
 
-        // fetch window manager object
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        // set layout parameter of window manager
 
         int LAYOUT_FLAG;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -58,9 +49,9 @@ public class TouchManager extends IntentService implements View.OnTouchListener 
         }
 
         WindowManager.LayoutParams mParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.MATCH_PARENT, // width is equal to full screen
-                WindowManager.LayoutParams.MATCH_PARENT, // height is equal to full screen
-                LAYOUT_FLAG , // Type Phone, These are non-application windows providing user interaction with the phone (in particular incoming calls).
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                LAYOUT_FLAG ,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, // this window won't ever get key input focus
                 PixelFormat.TRANSLUCENT);
         mParams.gravity = Gravity.START | Gravity.TOP;
